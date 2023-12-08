4d9daa0aee1c663a1b8d21769420a89836438ab2
effaroukk <efqay1@gmail.com>
2023-12-08 23:10:20 +0000

    gotit

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,2 +1,3 @@
-def complex_delete(a_dictionary, value):def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
+#!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
