6fc1a4ff2e049c48a3be603ee6cf575dc6169819
effaroukk <efqay1@gmail.com>
2024-05-02 14:10:19 +0100

    java

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const headerElem = document.querySelector('HEADER');
+  headerElem.style.color = '#FF0000';
+});document.addEventListener('DOMContentLoaded', function () {
+  const headerElem = document.querySelector('HEADER');
+  headerElem.style.color = '#FF0000';
+});
